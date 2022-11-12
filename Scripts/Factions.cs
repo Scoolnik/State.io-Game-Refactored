13d4dc399300721610c060ebe8257f97e247f32d
Scoolnik <62171058+Scoolnik@users.noreply.github.com>
2022-11-12 19:15:49 +0500

    bugfix
    fixed Factions dict initialization order

--- Scripts/Factions.cs
+++ Scripts/Factions.cs
@@ -13,10 +13,13 @@ namespace StateIO
 
 		private Dictionary<FactionId, Faction> _factionsDict;
 
-		private void OnEnable() => _factionsDict = _factions.ToDictionary(x => x.Id);
-
 		public FactionId GetRandom() => _factions[Random.Range(0, _factions.Length)].Id;
 
-		public Faction GetFactionInfo(FactionId id) => _factionsDict[id];
+		public Faction GetFactionInfo(FactionId id)
+		{
+			if (_factionsDict == null)
+				_factionsDict = _factions.ToDictionary(x => x.Id);
+			return _factionsDict[id];
+		}
 	}
 }
